74c66ee2ce6c6bc4b01bef28fd93eab1c5217e07
ellasophi4 <114925031+ellasophi4@users.noreply.github.com>
2024-07-11 11:57:47 +0100

    Update LightSwitch.cs

--- Assets/LightSwitch.cs
+++ Assets/LightSwitch.cs
@@ -1,46 +1,45 @@
+Script:
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightSwitch : MonoBehaviour
+public class lightswitch : MonoBehaviour
 {
-    public GameObject intIcon, lightOn, switchOn, 
-    lightOff, switchOn, switchOff;
-
+    public GameObject intIcon, lightOn, lightOff, switchOn, switchOff;
     public bool toggle;
-
     public AudioSource switchSound;
 
-        void OnTriggerStay(Collider other){
-            if (other.CompareTag("MainCamera"))
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
         {
-
-        intIcon.SetActive(true);
-        if (Input. GetKeyDown(KeyCode.E))
-        {
-            if(toggle == true)
+            intIcon.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if(toggle == true)
                 {
-                lightOn.SetActive(true);
-                lightOff.SetActive(false);
-                switchOn.SetActive(true);
-                switchOff.SetActive(false);
-                //switchSound.Play();
+                    lightOn.SetActive(true);
+                    lightOff.SetActive(false);
+                    switchOn.SetActive(true);
+                    switchOff.SetActive(false);
+                    //switchSound.Play();
                 }
-
-            if(toggle==false);
+                if (toggle == false)
                 {
-                lightOn.SetActive(false);
-                lightOff.SetActive(true);
-                switchOn.SetActive(false);
-                switchOff.SetActive(true);
+                    lightOn.SetActive(false);
+                    lightOff.SetActive(true);
+                    switchOn.SetActive(false);
+                    switchOff.SetActive(true);
+                    //switchSound.Play();
                 }
-        }
-        }
-        }
-
-        void OnTriggerExit(Collider other){
-            if(other.CompareTag("MainCamera")){
-                intIcon.SetActive(false);
             }
         }
     }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            intIcon.SetActive(false);
+        }
+    }
+}
