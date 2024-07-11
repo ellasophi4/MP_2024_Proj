6d1afdb8f25c76a03b59a0433328c69b2c92027a
ellasophi4 <114925031+ellasophi4@users.noreply.github.com>
2024-07-11 11:38:16 +0100

    Update LightSwitch.cs

--- Assets/LightSwitch.cs
+++ Assets/LightSwitch.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class LightSwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject intIcon, lightOn, switchOn, 
+    lightOff, switchOn, switchOff;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public bool toggle;
+
+    public AudioSource switchSound;
+
+        void OnTriggerStay(Collider other){
+            if (other.CompareTag("MainCamera"))
+        {
+
+        intIcon.SetActive(true);
+        if (Input. GetKeyDown(KeyCode.E))
+        {
+            if(toggle == true)
+                {
+                lightOn.SetActive(true);
+                lightOff.SetActive(false);
+                switchOn.SetActive(true);
+                switchOff.SetActive(false);
+                //switchSound.Play();
+                }
+
+            if(toggle==false);
+                {
+                lightOn.SetActive(false);
+                lightOff.SetActive(true);
+                switchOn.SetActive(false);
+                switchOff.SetActive(true);
+                }
+        }
+        }
+        }
+
+        void OnTriggerExit(Collider other){
+            if(other.CompareTag("MainCamera")){
+                intIcon.SetActive(false);
+            }
+        }
     }
-}
