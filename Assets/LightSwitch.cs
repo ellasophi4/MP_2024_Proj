ac3dc47a8b9bdd41d81576f71ba33554fc9b3f01
ellasophi4 <114925031+ellasophi4@users.noreply.github.com>
2024-07-15 10:54:48 +0100

    argh

--- Assets/LightSwitch.cs
+++ Assets/LightSwitch.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class lightswitch : MonoBehaviour
+public class LightSwitch : MonoBehaviour
 {
     public GameObject intIcon, lightOn, lightOff, switchOn, switchOff;
     public bool toggle;
